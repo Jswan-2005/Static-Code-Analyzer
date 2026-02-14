6f9a5e37bc1e420eb05dc12d45640f9e4e3424d1
Jonathan <johnnyswan2005@yahoo.com.au>
2026-02-14 14:15:30 +1100

    Added unused import checking

--- StaticCodeAnalyzer.py
+++ StaticCodeAnalyzer.py
@@ -4,6 +4,8 @@ import sys
 assignment_list = []
 load_list = []
 unused_var_list = []
+import_list = []
+used_import_list = []
 
 with open("main.py") as f:
     tree = ast.parse(f.read())
@@ -19,13 +21,19 @@ class Visitor(ast.NodeVisitor):
             if isinstance(targets, ast.Name):
                 if targets.id not in assignment_list:
                     assignment_list.append((targets.id, targets.lineno))
-
         self.generic_visit(node)
 
     def visit_Load(self, node):
         for node in ast.walk(tree):
             if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Load):
                 load_list.append(node.id)
+
+    def visit_Import(self, node):
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Import):
+                for alias in node.names:
+                    if ((str(alias.name)) not in import_list):
+                        import_list.append(str(alias.name))
 
 def find_unused_variables():
     Visitor().visit(tree)
@@ -34,13 +42,31 @@ def find_unused_variables():
             unused_var_list.append(assignment)
     return unused_var_list
 
+def find_used_import_list():
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Name):
+            if node.id in import_list and node.id not in used_import_list:
+                used_import_list.append(node.id)
+
 def main():
+    '''Unused Variable Checker'''
     unused_var_list = find_unused_variables()
     if unused_var_list:
         for var in unused_var_list:
             print(f"Variable {var[0]} declared at line {var[1]} is not used")
     else:
         print("No unused variables found")
+    '''Unused Import Checker'''
+    find_used_import_list()
+    if len(import_list) == len(used_import_list):
+        print("No unused imports found")
+    else:
+        for _import in import_list:
+            if _import not in used_import_list:
+                print(f"Unused import {_import}")
+
+
+
         
 if __name__ == "__main__":
     main()
